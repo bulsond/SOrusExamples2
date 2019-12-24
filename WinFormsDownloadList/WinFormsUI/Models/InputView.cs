82ee98c172ae76f76c0441e103aecbf64e462985
bulsond <bulsond@gmail.com>
2019-12-24 17:53:06 +0300

    new(InputView): класс ввода данных пользователем

---
+++ WinFormsDownloadList/WinFormsUI/Models/InputView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsUI.Models
+{
+    class InputView
+    {
+        public string Address { get; set; }
+        public string Parameter { get; set; }
+        public int From { get; set; }
+        public int To { get; set; }
+
+        public List<Item> GetItems()
+        {
+            List<Item> items = new List<Item>();
+
+
+            return items;
+        }
+    }
+}
