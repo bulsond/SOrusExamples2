484b80116d7ce4acb57c6964961595b2448aa243
bulsond <bulsond@gmail.com>
2019-12-24 17:48:56 +0300

    new(Item):  класс запроса и сохранения ответа

---
+++ WinFormsDownloadList/WinFormsUI/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WinFormsUI.Models
+{
+    public class Item
+    {
+        public string Address { get; }
+        public string Answer { get; set; }
+
+        public Item(string address)
+        {
+            if (string.IsNullOrEmpty(address))
+                throw new ArgumentException(nameof(address));
+
+            Address = address;
+            Answer = String.Empty;
+        }
+    }
+}
