4c772fe57a84654df6723696ec8aa833fb9afc70
bulsond <bulsond@gmail.com>
2019-12-24 17:23:44 +0300

    new(WinFormsDownloadList): создано решение

---
+++ WinFormsDownloadList/WinFormsUI/FormMain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsUI
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+
+            this.Text = "Пример";
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+    }
+}
