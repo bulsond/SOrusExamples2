b38a13d3133f38f2c3c29e9c75a3c3b29acadaf4
bulsond <bulsond@gmail.com>
2020-03-23 17:57:29 +0300

    DbWithProgress - получение данных из БД, запись данных в файл, отображение процесса с пом. ProgressBar

---
+++ DbWithProgress/WindowsFormsUI/FormMain.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WindowsFormsUI
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._textBoxFile = new System.Windows.Forms.TextBox();
+            this._buttonSelectFile = new System.Windows.Forms.Button();
+            this._progressBar = new System.Windows.Forms.ProgressBar();
+            this._labelProc = new System.Windows.Forms.Label();
+            this._buttonSave = new System.Windows.Forms.Button();
+            this._saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // _textBoxFile
+            // 
+            this._textBoxFile.Location = new System.Drawing.Point(23, 21);
+            this._textBoxFile.Name = "_textBoxFile";
+            this._textBoxFile.ReadOnly = true;
+            this._textBoxFile.Size = new System.Drawing.Size(364, 20);
+            this._textBoxFile.TabIndex = 0;
+            // 
+            // _buttonSelectFile
+            // 
+            this._buttonSelectFile.Location = new System.Drawing.Point(409, 19);
+            this._buttonSelectFile.Name = "_buttonSelectFile";
+            this._buttonSelectFile.Size = new System.Drawing.Size(75, 23);
+            this._buttonSelectFile.TabIndex = 1;
+            this._buttonSelectFile.Text = "Выбрать";
+            this._buttonSelectFile.UseVisualStyleBackColor = true;
+            // 
+            // _progressBar
+            // 
+            this._progressBar.Location = new System.Drawing.Point(23, 119);
+            this._progressBar.Name = "_progressBar";
+            this._progressBar.Size = new System.Drawing.Size(312, 23);
+            this._progressBar.TabIndex = 2;
+            // 
+            // _labelProc
+            // 
+            this._labelProc.AutoSize = true;
+            this._labelProc.Location = new System.Drawing.Point(341, 124);
+            this._labelProc.Name = "_labelProc";
+            this._labelProc.Size = new System.Drawing.Size(21, 13);
+            this._labelProc.TabIndex = 3;
+            this._labelProc.Text = "0%";
+            // 
+            // _buttonSave
+            // 
+            this._buttonSave.Location = new System.Drawing.Point(409, 119);
+            this._buttonSave.Name = "_buttonSave";
+            this._buttonSave.Size = new System.Drawing.Size(75, 23);
+            this._buttonSave.TabIndex = 4;
+            this._buttonSave.Text = "Сохранить";
+            this._buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // _saveFileDialog
+            // 
+            this._saveFileDialog.Filter = "Текстовые файлы|*.txt|Все файлы|*.*";
+            this._saveFileDialog.InitialDirectory = "D:\\";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 278);
+            this.Controls.Add(this._buttonSave);
+            this.Controls.Add(this._labelProc);
+            this.Controls.Add(this._progressBar);
+            this.Controls.Add(this._buttonSelectFile);
+            this.Controls.Add(this._textBoxFile);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Пример с ProgressBar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox _textBoxFile;
+        private System.Windows.Forms.Button _buttonSelectFile;
+        private System.Windows.Forms.ProgressBar _progressBar;
+        private System.Windows.Forms.Label _labelProc;
+        private System.Windows.Forms.Button _buttonSave;
+        private System.Windows.Forms.SaveFileDialog _saveFileDialog;
+    }
+}
+
---
+++ DbWithProgress/WindowsFormsUI/FormMain.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsUI
+{
+    public partial class FormMain : Form
+    {
+        private const string _conString = @"Data Source=(localdb)\MSSQLLocalDB;" +
+            "Initial Catalog=AdventureWorks2016;Integrated Security=True;" +
+            "Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;" +
+            "ApplicationIntent=ReadWrite;MultiSubnetFailover=False"; 
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+            _buttonSelectFile.Click += ButtonSelectFile_Click;
+            _buttonSave.Click += ButtonSave_Click;
+        }
+
+        private void ButtonSelectFile_Click(object sender, EventArgs e)
+        {
+            if (_saveFileDialog.ShowDialog() != DialogResult.OK)
+                return;
+
+            _textBoxFile.Text = _saveFileDialog.FileName;
+        }
+
+        private async void ButtonSave_Click(object sender, EventArgs e)
+        {
+            var file = _textBoxFile.Text;
+            if (String.IsNullOrEmpty(file))
+                return;
+
+            //кнопки
+            _buttonSave.Enabled = false;
+            _buttonSelectFile.Enabled = false;
+
+            //колбек отображения процесса записи в файл
+            IProgress<int> progress = new Progress<int>(value =>
+            {
+                _progressBar.Value = value;
+                _labelProc.Text = $"{value}%";
+                _labelProc.Update();
+            });
+            
+            try
+            {
+                await SaveTableAsync(progress, file);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                //кнопки
+                _buttonSave.Enabled = true;
+                _buttonSelectFile.Enabled = true;
+            }
+        }
+
+        private async Task SaveTableAsync(IProgress<int> progress, string file)
+        {
+            //двусоставный запрос к БД
+            var request = @"SELECT COUNT(AddressID)
+                           FROM[AdventureWorks2016].[Person].[Address];
+                           SELECT AddressLine1
+		                   FROM[AdventureWorks2016].[Person].[Address]";
+
+            using (var con = new SqlConnection(_conString))
+            using (var cmd = con.CreateCommand())
+            {
+                cmd.CommandText = request;
+                await con.OpenAsync();
+                using (var reader = await cmd.ExecuteReaderAsync())
+                {
+                    //получаем количество записей в таблице
+                    int countRecords = 0;
+                    if (reader.HasRows)
+                    {
+                        await reader.ReadAsync();
+                        countRecords = reader.GetInt32(0);
+                    }
+                    //записываем данные из таблицы в файл
+                    if (await reader.NextResultAsync())
+                    {
+                        //открываем файл на запись
+                        using (var fs = File.OpenWrite(file))
+                        {
+                            //текущая запись (строка) в таблице
+                            int currentRecord = 0;
+                            //пока есть записи в таблице
+                            while (await reader.ReadAsync())
+                            {
+                                //получаем второй столбец
+                                var column = reader.GetString(0);
+                                var bytes = Encoding.UTF8.GetBytes(column);
+                                //пишем в файл
+                                await fs.WriteAsync(bytes, 0, bytes.Length);
+
+                                //вычисляем процент записанных записей
+                                var percent = ++currentRecord * 100 / countRecords;
+                                //обновляем прогрессбар и лейбл
+                                progress?.Report(percent);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
